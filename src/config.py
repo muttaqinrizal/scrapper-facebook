2d574e3da5c381fedcc3617e3f51c8e068acbefc
muttaqinrizal <rizalm57@gmail.com>
2023-09-20 15:53:33 +0700

    initia

---
+++ src/config.py
@@ -0,0 +1,5 @@
+username = "develop.azura@gmail.com"
+password = "KawusWog12345"
+number_posts_max = 30 #Number of Posts to Scrape
+number_comments_max = 0 #Number of Comments to Extract by each Post
+output_file_name = 'fb_scraper_output.csv'
